f606bbdadcc92fa14b71f7e7ff291dbd0f9653b4
William Sedlacek <wsedlacekc@gmail.com>
2019-09-30 16:19:21 -0500

    Setup Search
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/components/todo-form/todo-form.component.tsx
+++ src/app/components/todo-form/todo-form.component.tsx
@@ -4,18 +4,21 @@ import { Button, TextField, Card, CardContent, CardActions } from '@material-ui/
 type TodoFormProps = {
   onSubmit: (title: string) => void;
   onClear: () => void;
+  onSearch: (search: string) => void;
 };
 type TodoFormState = {
   title: string;
+  search: string;
 };
 
 class TodoForm extends React.Component<TodoFormProps, TodoFormState> {
   constructor(props: TodoFormProps) {
     super(props);
 
-    this.state = { title: '' };
+    this.state = { title: '', search: '' };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
   }
 
   handleSubmit(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
@@ -25,9 +28,15 @@ class TodoForm extends React.Component<TodoFormProps, TodoFormState> {
     if (title) onSubmit(title);
   }
 
+  handleSearch(e: React.ChangeEvent<HTMLInputElement>) {
+    const { onSearch } = this.props;
+    this.setState({ search: e.target.value });
+    onSearch(e.target.value);
+  }
+
   render() {
     const { onClear } = this.props;
-    const { title } = this.state;
+    const { title, search } = this.state;
 
     return (
       <Card>
@@ -37,6 +46,7 @@ class TodoForm extends React.Component<TodoFormProps, TodoFormState> {
             value={title}
             onChange={(e) => this.setState({ title: e.target.value })}
           />
+          <TextField label='Search' value={search} onChange={this.handleSearch} />
         </CardContent>
         <CardActions>
           <Button onClick={this.handleSubmit}>Add Todo</Button>
--- src/app/pages/todo/todo.page.tsx
+++ src/app/pages/todo/todo.page.tsx
@@ -4,10 +4,14 @@ import { TodoForm } from '../../components/todo-form/todo-form.component';
 import { TodoList } from '../../components/todo-list/todo-list.component';
 import { Todo } from '../../models/Todo';
 
+import { BehaviorSubject } from 'rxjs';
+
 type TodoProps = {};
 type TodoState = {
   itterator: number;
-  todos: Todo[];
+  shownTodos: Todo[];
+  todos: BehaviorSubject<Todo[]>;
+  search: BehaviorSubject<string>;
 };
 
 class TodoPage extends React.Component<TodoProps, TodoState> {
@@ -23,27 +27,47 @@ class TodoPage extends React.Component<TodoProps, TodoState> {
     }
 
     this.state = {
-      itterator: 0,
-      todos: storedTodos,
+      itterator: storedTodos.length,
+      todos: new BehaviorSubject(storedTodos),
+      shownTodos: storedTodos,
+      search: new BehaviorSubject(''),
     };
 
     this.updateTodos = this.updateTodos.bind(this);
     this.createTodo = this.createTodo.bind(this);
     this.toggleTodo = this.toggleTodo.bind(this);
     this.clearTodos = this.clearTodos.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  public componentDidMount() {
+    //Effects (RXJS)
+    this.state.search.subscribe((search) => {
+      const todos = this.state.todos.getValue();
+      this.setState({
+        ...this.state,
+        shownTodos: todos.filter((todo) => todo.task.includes(search)),
+      });
+    });
+
+    this.state.todos.subscribe((todos) => {
+      const search = this.state.search.getValue();
+      this.setState({
+        ...this.state,
+        shownTodos: todos.filter((todo) => todo.task.includes(search)),
+      });
+    });
   }
 
   private updateTodos(todos: Todo[]) {
-    this.setState({
-      ...this.state,
-      todos: [...todos],
-    });
-
+    this.state.todos.next(todos);
     localStorage.setItem('todos', JSON.stringify(todos));
   }
 
   private createTodo(title: string) {
-    const { itterator, todos } = this.state;
+    const { itterator } = this.state;
+    const todos = this.state.todos.getValue();
+
     const newTodo: Todo = {
       id: itterator,
       completed: false,
@@ -57,7 +81,7 @@ class TodoPage extends React.Component<TodoProps, TodoState> {
   }
 
   private toggleTodo(newTodo: Todo) {
-    const { todos } = this.state;
+    const todos = this.state.todos.getValue();
 
     const newTodos = todos.map((todo) => {
       if (todo.id === newTodo.id) todo.completed = !todo.completed;
@@ -68,18 +92,26 @@ class TodoPage extends React.Component<TodoProps, TodoState> {
   }
 
   private clearTodos() {
-    const { todos } = this.state;
+    const todos = this.state.todos.getValue();
     const newTodos = todos.filter((todo) => !todo.completed);
     this.updateTodos([...newTodos]);
   }
 
+  private handleSearch(search: string) {
+    this.state.search.next(search);
+  }
+
   public render() {
-    const { todos } = this.state;
+    const { shownTodos } = this.state;
 
     return (
       <React.Fragment>
-        <TodoForm onSubmit={this.createTodo} onClear={this.clearTodos} />
-        <TodoList todos={todos} onToggle={this.toggleTodo} />
+        <TodoForm
+          onSubmit={this.createTodo}
+          onClear={this.clearTodos}
+          onSearch={this.handleSearch}
+        />
+        <TodoList todos={shownTodos} onToggle={this.toggleTodo} />
       </React.Fragment>
     );
   }
